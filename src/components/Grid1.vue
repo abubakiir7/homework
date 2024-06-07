46245cb68e5fd3fcfe31f5a5147e2ec415660a10
abubakiir7 <miraxmedovabubakr@gmail.com>
2024-06-07 16:15:08 +0500

    ass

--- src/components/Grid1.vue
+++ src/components/Grid1.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="bg-white p-10" >
-        <div  >
-            <img class="pb-8" :src="item.img" alt="">
-        </div>
-        <h3 class="text-[24px] text-[#282938] leading-[36px]" >{{item.title}}</h3>
-        <p class="leading-[28px] text-[#282938] text-[17px] py-4" >{{item.text}}</p>
+  <div class="bg-white p-10">
+    <div class="flex justify-center md:justify-start">
+      <img class="pb-8" :src="item.img" alt="" />
     </div>
+    <h3 class="text-[24px] text-[#282938] leading-[36px]">{{ item.title }}</h3>
+    <p class="leading-[28px] text-[#282938] text-[17px] py-4">
+      {{ item.text }}
+    </p>
+  </div>
 </template>
 
 <script setup>
-    const props = defineProps({
-        item: Object
-    })
+const props = defineProps({
+  item: Object,
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
